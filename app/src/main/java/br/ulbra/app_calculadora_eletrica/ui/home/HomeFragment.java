942d57d4c9f8ece38963c7b97ffa68921cc0de5e
unknown <Administrador@CSL-N188903.ensino.ulbra.net>
2025-10-21 22:11:51 -0300

    Refatoração e organização do dados

--- app/src/main/java/br/ulbra/app_calculadora_eletrica/ui/home/HomeFragment.java
+++ app/src/main/java/br/ulbra/app_calculadora_eletrica/ui/home/HomeFragment.java
@@ -27,7 +27,7 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_lei_ohm, container, false);
 
         initializeViews(root);
         setupObservers();
